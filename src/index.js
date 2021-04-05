708e1c24af315a78eef518e3688360119d31d38a
Xiaochen Xu <xiaochex@gmail.com>
2021-04-05 18:34:09 -0400

    populated clickable link with structured contents

--- src/index.js
+++ src/index.js
@@ -13,8 +13,38 @@ function getCountries(){
         console.log(data)
         data.map(country => {
             ul.innerHTML += `
-            <li><a href="#">${country.name}</a></li>
+            <li><a href="#" data-name="${country.name}">${country.name}</a></li>
             `
         })
+        clickableLinks()
     })
 }
+
+function clickableLinks(){
+    let ul = document.getElementById("country-list")
+    ul.addEventListener("click",displayCountry)
+}
+
+function displayCountry(e){
+    console.log(e)
+    let ul = document.getElementById("country-list")
+    ul.innerHTML = ""
+    let div = document.getElementById("show-country")
+    fetch(BASE_URL + `/name/${e.target.dataset.name}`)
+    .then(res => res.json())
+    .then(country => {
+        console.log(country)
+        div.innerHTML = `
+        <h1>${country[0].name}</h1>
+        <h3>Capital: </h3><p>${country[0].capital}</p>
+        <h4>Native Name: </h4><p>${country[0].nativeName}</p>
+        <h4>Population: </h4><p>${country[0].population}</p>
+        <h4>Region: </h4><p>${country[0].region}</p>
+        <h4>Currency: </h4><p>${country[0].currencies[0].name}</p>
+        <h4>National Flag<h4>
+        <img src="${country[0].flag}" alt="Country flag" width="450" height="300">
+        `
+    })
+}
+
+
