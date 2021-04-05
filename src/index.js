8daa52c37487524464b194b0723fa8ad737067ed
Xiaochen Xu <xiaochex@gmail.com>
2021-04-05 16:52:24 -0400

    able to list all country names on DOM

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function getCountries(){
+    let ul = document.getElementById("country-list")
+    ul.innerHTML = ""
     fetch("https://restcountries.eu/rest/v2/all")
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        data.map(country => {
+            ul.innerHTML += `
+            <li>${country.name}</li>
+            `
+        })
     })
 }
