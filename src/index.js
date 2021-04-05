7aa409b1ec4e1691323944dfe60067daf4b6e889
Xiaochen Xu <xiaochex@gmail.com>
2021-04-05 16:30:05 -0400

    fetch added in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     getCountries()  
 })
+
+function getCountries(){
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
