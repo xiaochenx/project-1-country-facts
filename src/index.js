43736e2d99c4c4136d022c7ed9c48da8291222e0
Xiaochen Xu <xiaochex@gmail.com>
2021-04-06 11:20:10 -0400

    moved img tag up and changed size

--- src/index.js
+++ src/index.js
@@ -41,14 +41,13 @@ function displayCountry(e){
     .then(country => {
         console.log(country)
         div.innerHTML = `
-        <h1>${country[0].name}</h1>
+        <h1>${country[0].name}</h1><img src="${country[0].flag}" alt="Country flag" width="350" height="250">
         <h3>Capital: </h3><p>${country[0].capital}</p>
         <h4>Native Name: </h4><p>${country[0].nativeName}</p>
         <h4>Population: </h4><p>${country[0].population}</p>
         <h4>Region: </h4><p>${country[0].region}</p>
         <h4>Currency: </h4><p>${country[0].currencies[0].name}</p>
         <h4>National Flag<h4>
-        <img src="${country[0].flag}" alt="Country flag" width="450" height="300">
         `
     })
 }
